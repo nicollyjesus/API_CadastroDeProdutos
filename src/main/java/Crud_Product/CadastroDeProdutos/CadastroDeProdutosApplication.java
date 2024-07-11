84e8e9443ed657cc3442d590afc79849473407c3
nicolly_jesus <128162519+nicollyjesus@users.noreply.github.com>
2024-07-11 01:02:08 -0300

    "criação da api utilizando sql"

---
+++ src/main/java/Crud_Product/CadastroDeProdutos/CadastroDeProdutosApplication.java
@@ -0,0 +1,13 @@
+package Crud_Product.CadastroDeProdutos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeProdutosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Crud_Product/CadastroDeProdutos/CadastroDeProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package Crud_Product.CadastroDeProdutos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
